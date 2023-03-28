82614d54a64554c9a78b919e2d3a32c49f190ac1
TheRealAGBOOLA <75374908+Agboolasam@users.noreply.github.com>
2023-03-28 11:46:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
   let counts = 0;
   let new_counts = document.getElementById("count-el");
   let prevCount = document.getElementById("prev_cnt");
+let saveBtn = document.getElementById("Save-btn");
 
   function increment() {
       counts = counts + 1;
       console.log(counts);
       new_counts.innerText = counts;
+      saveBtn.disabled = False;
   }
 
 function clear(){
 
     counts = 0;
-
-     new_counts.innerText = counts;
-
+    new_counts.innerText = counts;
+    saveBtn.disabled = True;
 }
 
   function store() {
       prevCount.innerText = "Previous count is" + " " + counts;
      clear()
-
   }
 
 
